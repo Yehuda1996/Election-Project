b7ad53b0a7a6204c0e9dd18a53dfe20da07fefe5
Yehuda1996 <ygrajower@gmail.com>
2024-11-12 12:09:54 +0200

    updated voteController

--- Backend/src/controllers/voteController.ts
+++ Backend/src/controllers/voteController.ts
@@ -1,27 +1,33 @@
 import { Request, Response } from "express";
 import CandidateModel from "../models/CandidateModel";
 import UserModel from "../models/UserModel";
-
+import { VoteDto } from "../dto/voteDto";
 
 export const handleVote = async (req: Request, res: Response) => {
     try {
-        const {userId, candidateId} = req.body;
+        const { userId, candidateId }: VoteDto = req.body;
+
+        const user = await UserModel.findById(userId);
+        const candidate = await CandidateModel.findById(candidateId);
+        if (!user) return res.status(404).json({ message: "User not found" });
+
+        if (user.hasVoted && user.votedFor !== candidate) {
+            await CandidateModel.findByIdAndUpdate(user.votedFor, {
+                $inc: { votes: -1 },
+            });
+        }
+
         await CandidateModel.findByIdAndUpdate(candidateId, {
-            $inc: {
-                votes: 1
-            }
+            $inc: { votes: 1 },
         });
+
         await UserModel.findByIdAndUpdate(userId, {
-            $inc: {
-                hasVoted: true,
-                votedFor: candidateId
-            }
+            hasVoted: true,
+            votedFor: candidateId,
         });
-        res.status(200).json({message: "Voting complete!"})
-        return;
-    } 
-    catch (error) {
-        res.status(500).json(error);
-        return;
+
+        res.status(200).json({ message: "Vote updated successfully!" });
+    } catch (error) {
+        res.status(500).json({ error: "Voting failed", details: error });
     }
-}
+};
